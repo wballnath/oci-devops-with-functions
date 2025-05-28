1f10ada6fc8ad6c0c9ad99de1563ef101f6fd664
wballnath <58166508+wballnath@users.noreply.github.com>
2025-05-28 15:29:53 +0200

    Update providers.tf
    Update Terraform provider to deploy via ORM

--- providers.tf
+++ providers.tf
@@ -25,22 +25,32 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  region       = var.region
+            tenancy-ocid = var.tenancy-ocid
+            region       = var.region
 
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-}
+            }
+
+            provider "oci" {
+            alias        = "home-region"
+            tenancy-ocid = var.tenancy-ocid
+            region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
+
+
+            }
+
+            provider "oci" {
+            alias        = "current-region"
+            tenancy-ocid = var.tenancy-ocid
+            region       = var.region
+
+
+            }
 
 provider "oci" {
   alias        = "home_region"
   tenancy_ocid = var.tenancy_ocid
   region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
 
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
 }
 
 provider "oci" {
@@ -48,7 +58,5 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
 
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+ 
 }
